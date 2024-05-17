ed9701282a9ac5fe6b23b6e0441c669b501a173e
alixdemerto <i.aslan2020@gtu.edu.tr>
2024-05-17 21:42:38 +0300

    Organise folders & Add state machine

---
+++ BogaziciJam/Assets/Scripts/StateMachine/State.cs
@@ -0,0 +1,52 @@
+using IboshEngine.Runtime.Interfaces;
+using UnityEngine;
+
+namespace StateMachine
+{
+    public abstract class State<T1, T2> : ILogicUpdate where T1: MonoBehaviour where T2: ScriptableObject 
+    {
+        protected T1 obj;
+        protected T2 objData;
+        protected StateMachine<T1, T2> stateMachine;
+        protected float startingTime;
+        protected bool isAnimationFinished;
+        protected readonly string animBoolName;
+
+        public State(T1 obj, StateMachine<T1, T2> stateMachine, T2 objData, string animBoolName = null)
+        {
+            this.obj = obj;
+            this.stateMachine = stateMachine;
+            this.objData = objData;
+            this.animBoolName = animBoolName;
+        }
+
+        public virtual void Enter()
+        {
+            startingTime = Time.time;
+            isAnimationFinished = false;
+            DoChecks();
+        }
+        
+        public virtual void Exit()
+        {
+            
+        }
+        
+        public virtual void DoChecks()
+        {
+            
+        }
+        
+        public virtual void LogicUpdate()
+        {
+            
+        }
+        
+        public virtual void PhysicsUpdate()
+        {
+            DoChecks();
+        }
+
+        public void AnimationFinishTrigger() => isAnimationFinished = true;
+    }
+}
---
+++ BogaziciJam/Assets/Scripts/StateMachine/StateMachine.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+
+namespace StateMachine
+{
+    public class StateMachine<T1, T2> where T1 : MonoBehaviour where T2 : ScriptableObject
+    {
+        public Action OnStateChanged;
+        public State<T1, T2> CurrentState { get; private set; }
+        public State<T1, T2> PreviousState { get; private set; }
+
+        public void Initialize(State<T1, T2> startingState)
+        {
+            CurrentState = startingState;
+            CurrentState.Enter();
+            OnStateChanged?.Invoke();
+        }
+        
+        public void ChangeState(State<T1, T2> newState)
+        {
+            PreviousState = CurrentState;
+            CurrentState.Exit();
+            CurrentState = newState;
+            CurrentState.Enter();
+            OnStateChanged?.Invoke();
+        }
+    }
+}
